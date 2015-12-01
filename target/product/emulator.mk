964dc89f2889ae42990c5e8b26190a5984fc6bcf
Louis Popi <theh2o64@gmail.com>
2015-12-01 11:12:37 +0100

    aosp_arm64: Yu devices aren't emulators
    Change-Id: I0c04c27fa4f78e16ad019fa7a0d9d0566f289ace
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- target/product/emulator.mk
+++ target/product/emulator.mk
@@ -18,6 +18,7 @@
 # emulator-related modules to PRODUCT_PACKAGES.
 #
 
+ifneq ($(PRODUCT_BRAND),YU)
 # Host modules
 PRODUCT_PACKAGES += \
 
@@ -67,3 +68,4 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGE_OVERLAYS := device/generic/goldfish/overlay
 
 PRODUCT_CHARACTERISTICS := emulator
+endif
